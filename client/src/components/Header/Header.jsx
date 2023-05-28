b804a4cd27720ee42d53ad281d98ab994cb4bfd7
Sai Aung Min Khant <saiaungminkhant20@gmail.com>
2023-05-28 11:55:30 +0630

    commit

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,6 +6,12 @@ function Header() {
     return (
         <div className="header">
             <Navbar />
+            <div className="promo">
+                <span>The Best Look</span><br />
+                <h1>Anytime Anywhere</h1>
+                <span>Starts from 10,000 MMK</span><br />
+                <button>View</button>
+            </div>
         </div>
     )
 }
